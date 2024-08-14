6513b2f058df4b38cf1635cbed4213a6c0788bf4
vdorbs <victor.d.dorobantu@gmail.com>
2024-08-14 12:07:03 -0400

    Lowered diffusion constant in heat method demo

--- demos/demo_heat_method.py
+++ demos/demo_heat_method.py
@@ -8,7 +8,7 @@ fs, faces = load_obj_from_url(SPOT_URL)
 m = Manifold(faces)
 fs -= m.embedding_to_com(fs)
 fs *= sqrt(4 * pi / m.embedding_to_face_areas(fs).sum())
-heat_method_solver = m.embedding_to_heat_method_solver(fs, use_diag_mass=True, diff_coeff=100)
+heat_method_solver = m.embedding_to_heat_method_solver(fs, use_diag_mass=True, diff_coeff=4.)
 
 source_idxs = tensor([0, 200, 400])
 all_dists = heat_method_solver(source_idxs)
