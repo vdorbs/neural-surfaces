21f6fdb25ec051e28243b1b291d7ef9de0908ccc
vdorbs <victor.d.dorobantu@gmail.com>
2024-08-20 13:48:59 -0400

    Added optional wandb logging to surface fitting demo

--- demos/demo_fit_surfaces.py
+++ demos/demo_fit_surfaces.py
@@ -5,21 +5,24 @@ from neural_surfaces.surface_models import SphericalSurfaceModel
 from neural_surfaces.utils import meshes_to_html, serve_html
 from os import environ, listdir
 from potpourri3d import read_mesh
-from torch import device, float64, no_grad, pi, save, sqrt, tensor, zeros, zeros_like
+from torch import device, float64, log10, no_grad, pi, save, sqrt, tensor, zeros
 from torch.cuda import set_device
 from torch.distributed import all_gather, all_gather_into_tensor, destroy_process_group, init_process_group
 from torch.linalg import norm
 from torch.multiprocessing import spawn
 from torch.optim import Adam
 from tqdm import tqdm
+import wandb
+from wandb import Html
 
 
 parser = ArgumentParser()
 parser.add_argument('--dataset_path', required=True)
-parser.add_argument('--output_path', required=True)
+parser.add_argument('--output_path')
 parser.add_argument('--surfaces', type=int, nargs='*')
 parser.add_argument('--devices', type=int, nargs='*')
 parser.add_argument('--num_epochs', type=int, default=100)
+parser.add_argument('--log', type=bool, default=False)
 args = parser.parse_args()
 
 names = [filename[:-4] for filename in listdir(f'{args.dataset_path}/params') if filename[-4:] == '.obj']
@@ -57,6 +60,9 @@ pbar = tqdm(total=args.num_epochs)
 def train(rank):
     # Need this otherwise extra process starts on default GPU
     set_device(devices[rank])
+
+    if rank == 0 and args.log:
+        wandb.init(config=args)
 
     environ['MASTER_ADDR'] = 'localhost'
     environ['MASTER_PORT'] = '12345'
@@ -105,7 +111,11 @@ def train(rank):
             pbar.update()
             pbar.set_description(f'loss={gathered_losses.mean().item()}')
 
-    save(model.state_dict, f'{args.output_path}/{names[rank]}.pt')
+            if args.log:
+                wandb.log({f'{name}/log_loss':log10(loss) for name, loss in zip(names, gathered_losses)})
+
+    if args.output_path is not None:
+        save(model.state_dict, f'{args.output_path}/{names[rank]}.pt')
 
     # Gather reconstructions across devices
     all_recon_fs = [zeros(*fs.shape, dtype=fs.dtype, device=devices[rank]) for fs in all_fs]
@@ -114,12 +124,20 @@ def train(rank):
     # No more gathers needed
     destroy_process_group()
 
-    # Serve reconstruction renderings
+    # Render reconstructions
     if rank == 0:
-        render_fs = [[recon_fs.cpu() for recon_fs in all_recon_fs]]
-        render_faces = [[manifold.faces.cpu() for manifold in all_manifolds]]
-        render_uvs = [[zeros(manifold.num_vertices, 2) for manifold in all_manifolds]]
-        serve_html(meshes_to_html(render_fs, render_faces, render_uvs, mode='none'), serve_locally=False)
+        all_render_fs = [recon_fs.cpu() for recon_fs in all_recon_fs]
+        all_render_faces = [manifold.faces.cpu() for manifold in all_manifolds]
+        all_render_uvs = [zeros(manifold.num_vertices, 2) for manifold in all_manifolds]
+
+        if args.log:
+            for name, render_fs, render_faces, render_uvs in zip(names, all_render_fs, all_render_faces, all_render_uvs):
+                wandb.log({f'{name}/recon': Html(meshes_to_html([[render_fs]], [[render_faces]], [[render_uvs]], mode='none'))})
+                
+            wandb.finish()
+
+        else:
+            serve_html(meshes_to_html([all_render_fs], [all_render_faces], [all_render_uvs], mode='none'), serve_locally=False)
 
 if __name__ == '__main__':
     spawn(train, nprocs=len(devices), join=True)
