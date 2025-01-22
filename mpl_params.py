e532ed83ee3f93cdaced96a9b37cd10109d1a422
vdorbs <victor.d.dorobantu@gmail.com>
2025-01-22 12:13:56 -0500

    Added mpl style

---
+++ mpl_params.py
@@ -0,0 +1,17 @@
+ana_dodik_style = {
+    "text.usetex": True,
+    "font.family": "sans-serif",
+    "font.serif": ["Linux Libertine"],
+    "font.size": 24,
+    "figure.facecolor": "#FCFCFC",
+    "axes.facecolor": "#FCFCFC",
+    # "axes.prop_cycle": cycler(color=qualitative_cmap.mpl_colors),
+    "lines.antialiased": True,
+    "text.latex.preamble": r"""
+    \usepackage{libertine}
+    \usepackage[libertine]{newtxmath}
+    """,
+    "mathtext.rm": "libertine",
+    "mathtext.it": "libertine:italic",
+    "mathtext.bf": "libertine:bold",
+}
